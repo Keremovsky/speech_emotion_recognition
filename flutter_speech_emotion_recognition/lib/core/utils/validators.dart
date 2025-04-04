b891aa0d44f963b0081be6ab3fe772d8103521c1
Keremovsky <kerem.egitim2022@gmail.com>
2025-04-04 22:51:09 +0300

    input validators are created and added to login view

--- flutter_speech_emotion_recognition/lib/core/utils/validators.dart
+++ flutter_speech_emotion_recognition/lib/core/utils/validators.dart
@@ -4,10 +4,46 @@ import 'package:flutter_speech_emotion_recognition/gen/locale_keys.g.dart';
 import 'package:fpdart/fpdart.dart';
 
 class InputFieldValidator {
+  static final List<String> _validDomains = ["gmail.com"];
+  static final String _emailPattern =
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
+  static final String _passwordPattern =
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$';
+
   static Option<ValueFailureModel> validateEmptiness(String? input) {
     if (input == null || input.isEmpty) {
       return some(ValueFailureModel.invalidInput(LocaleKeys.emptyFieldMessage.tr()));
     }
+
+    return none();
+  }
+
+  static Option<ValueFailureModel> validateEmail(String? input) {
+    if (input == null || input.isEmpty) {
+      return some(ValueFailureModel.invalidInput(LocaleKeys.emptyFieldMessage.tr()));
+    }
+
+    final RegExp regex = RegExp(_emailPattern);
+
+    if (!regex.hasMatch(input)) return some(ValueFailureModel.invalidInput(""));
+
+    final String domain = input.split("@").last;
+
+    if (!_validDomains.contains(domain)) {
+      return some(ValueFailureModel.invalidInput(""));
+    }
+
+    return none();
+  }
+
+  static Option<ValueFailureModel> validatePassword(String? input) {
+    if (input == null || input.isEmpty) {
+      return some(ValueFailureModel.invalidInput(LocaleKeys.emptyFieldMessage.tr()));
+    }
+
+    final RegExp regex = RegExp(_passwordPattern);
+
+    if (!regex.hasMatch(input)) return some(ValueFailureModel.invalidInput(""));
 
     return none();
   }
--- flutter_speech_emotion_recognition/lib/features/auth/state/login_view_state.dart
+++ flutter_speech_emotion_recognition/lib/features/auth/state/login_view_state.dart
@@ -2,23 +2,36 @@ import 'dart:developer';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speech_emotion_recognition/core/utils/validators.dart';
 import 'package:flutter_speech_emotion_recognition/features/auth/view/login_view.dart';
 import 'package:flutter_speech_emotion_recognition/router/router.dart';
 
 abstract class LoginViewState extends State<LoginView> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  String? email;
-  String? password;
+  late String email;
+  late String password;
 
-  void onEmailSaved(String? value) {}
-  String? onEmailValidate(String? value) {
-    return null;
+  void onEmailSaved(String? value) {
+    setState(() {
+      email = value!;
+    });
   }
 
-  void onPasswordSaved(String? value) {}
+  String? onEmailValidate(String? value) {
+    final control = InputFieldValidator.validateEmail(value);
+    return control.toNullable()?.message;
+  }
+
+  void onPasswordSaved(String? value) {
+    setState(() {
+      password = value!;
+    });
+  }
+
   String? onPasswordValidate(String? value) {
-    return null;
+    final control = InputFieldValidator.validatePassword(value);
+    return control.toNullable()?.message;
   }
 
   void onForgotPasswordPressed() {
